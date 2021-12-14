3f6843d8cbf7c90403a1344e439105159da60eb7
Olivier Crété <olivier.crete@epitech.eu>
2021-12-14 12:02:27 +0100

    Position type

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -7,3 +7,7 @@ export type PaperFlexPlacement = 'spaces' | 'edges' | 'right' | 'center' | 'left
 export type PaperAlign = 'left' | 'center' | 'right';
 
 export type Attributes = Record<string, string|boolean>;
+
+export type PositionY = 'top' | 'bottom';
+export type PositionX = 'left' | 'right';
+export type Position = PositionX | PositionY;
