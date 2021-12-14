0e53da04d4845ca6ad14088a5564a0cff3876941
Olivier Crété <olivier.crete@epitech.eu>
2021-12-14 14:22:39 +0100

    Factorize container css

--- src/lib/components/Toast/ToastContainer.svelte
+++ src/lib/components/Toast/ToastContainer.svelte
@@ -10,12 +10,10 @@ export let position: ToastPosition = 'top-right';
 </script>
 
 <style lang="scss">
-:global(.paper-toast-container.top > div),
-:global(.paper-toast-container.bottom > div) {
+:global(.paper-toast-container:is(.top, .bottom) > div)  {
   align-self: center;
 }
-:global(.paper-toast-container.top-right > div),
-:global(.paper-toast-container.bottom-right > div) {
+:global(.paper-toast-container:is([class*=right]) > div)  {
   align-self: end;
 }
 
@@ -25,35 +23,21 @@ div {
   width: 100%;
   padding: 2em;
   overflow: hidden;
-  z-index: 1000;
+  z-index: 9999;
   pointer-events: none;
-  // TODO:
   flex-direction: column;
-  // flex-direction: column-reverse;
-  &.top {
+
+  &[class*=top] {
     top: 0;
   }
 
-  &.top-right {
-    top: 0;
+  &[class*=bottom] {
+    bottom: 0;
+    flex-direction: column-reverse;
   }
 
-  &.top-left {
-    top: 0;
+  &[class*=left] {
     left: 0;
-  }
-
-  &.bottom {
-    bottom: 0;
-  }
-
-  &.bottom-right {
-    bottom: 0;
-  }
-
-  &.bottom-left {
-    left: 0;
-    bottom: 0;
   }
 }
 </style>
