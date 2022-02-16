6dcaa4f94932f43b6f3d7d930c43d2f449fa7dbd
Olivier <olivier.crete@epitech.eu>
2022-02-16 12:34:00 +0100

    CopyButton (#35)

---
+++ src/lib/components/CopyButton.svelte
@@ -0,0 +1,36 @@
+<Button {...$$restProps}
+        aria-label={ariaLabel}
+        on:click
+        on:click={copy}>
+  <slot />
+</Button>
+
+<script lang="ts">
+import { createEventDispatcher } from 'svelte';
+import Button from './Button.svelte';
+import { success } from './Toast';
+
+export let text: string;
+export let successMessage: string = 'Copied';
+export let ariaLabel: string = 'Copy to clipboard';
+
+const dispatch = createEventDispatcher();
+
+async function copy() {
+  if (!text) return;
+
+  try {
+    await navigator.clipboard.writeText(text);
+    dispatch('copy', text);
+
+    if (successMessage)
+      success({
+        message: successMessage,
+        position: 'top',
+        duration: 1000,
+      });
+  } catch(e) {
+    console.error(e);
+  }
+}
+</script>
