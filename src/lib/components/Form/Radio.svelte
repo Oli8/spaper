d074e013673410348b47a59437b11c738356e2d9
Olivier Crété <olivier.crete@epitech.eu>
2022-08-05 17:16:32 +0200

    fix conflicts

--- src/lib/components/Form/Radio.svelte
+++ src/lib/components/Form/Radio.svelte
@@ -1,15 +1,4 @@
-{#if label}
-  <label>
-    {label}
-    <input type="radio"
-           {value}
-           {...$$restProps}
-           bind:group
-           on:focus
-           on:blur
-           on:change />
-  </label>
-{:else}
+<label class="paper-radio">
   <input type="radio"
          {value}
          {...$$restProps}
@@ -17,9 +6,6 @@
          on:focus
          on:blur
          on:change />
-<<<<<<< HEAD
-{/if}
-=======
   <span>
     {#if $$slots.default}
       <slot />
@@ -28,10 +14,19 @@
     {/if}
   </span>
 </label>
->>>>>>> 5b570cd (Radio: add slot support (#56))
 
 <script lang="ts">
 export let label: string = '';
 export let value: string = '';
 export let group: string = '';
 </script>
+
+<style lang="scss">
+@import './style';
+
+input[disabled] {
+  + span::before, ~ span {
+    @extend %disabled;
+  }
+}
+</style>
