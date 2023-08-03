998afb2669121d0e20aeb325ac84a345da4a6dd7
Bernard Dröge <45244528+droege@users.noreply.github.com>
2023-08-03 20:36:06 +0200

    add Row.svelte & Col.svelte

---
+++ src/lib/components/Flexgrid/Col.svelte
@@ -0,0 +1,38 @@
+﻿<div {...props}>
+  <slot />
+</div>
+
+<script lang="ts">
+  type colSpan = number | string | undefined;
+
+  export let col: colSpan = undefined;
+  export let sm: colSpan = undefined;
+  export let md: colSpan = undefined;
+  export let lg: colSpan = undefined;
+
+  export let fill: boolean = false;
+  export let top: boolean = false;
+  export let middle: boolean = false;
+  export let bottom: boolean = false;
+
+  const breakpoints = ['col', 'sm', 'md', 'lg'];
+
+  const breakpointClasses = [col, sm, md, lg].map((value, i) => {
+    const breakpoint = breakpoints[i];
+    return value === undefined
+      ? ''
+      : breakpoint + '-' + value;
+  });
+
+  const utilities = ['col-fill', 'align-top', 'align-middle', 'align-bottom'];
+
+  const utilityClasses = [fill, top, middle, bottom].map(
+    (value, i) => value ? utilities[i] : ''
+  );
+
+  const props = {
+    ...$$restProps,
+    class: [$$restProps.class, 'col', ...breakpointClasses, ...utilityClasses]
+      .join(' ')
+  };
+</script>
---
+++ src/lib/components/Flexgrid/Row.svelte
@@ -0,0 +1,28 @@
+﻿<div {...props}>
+  <slot />
+</div>
+
+<script lang="ts">
+  export let right: boolean = false;
+  export let center: boolean = false;
+  export let edges: boolean = false;
+  export let spaces: boolean = false;
+  export let top: boolean = false;
+  export let middle: boolean = false;
+  export let bottom: boolean = false;
+
+  const utilities = ['flex-right', 'flex-center', 'flex-edges', 'flex-spaces', 'flex-top', 'flex-middle', 'flex-bottom'];
+
+  const utilityClasses = [right, center, edges, spaces, top, middle, bottom].map(
+    (value, i) => value ? utilities[i] : ''
+  );
+
+  const props = {
+    ...$$restProps,
+    class: [
+      $$restProps.class,
+      'row',
+      ...utilityClasses
+    ].join(' ')
+  };
+</script>
