d3f1788b6d2ea4de78e94894c813a5c778eb2661
Olivier Crété <olivier.crete@epitech.eu>
2024-02-29 18:20:52 +0100

    NumberInput in/decrement components script formatting

--- src/lib/components/Form/NumberInput/DecrementButton.svelte
+++ src/lib/components/Form/NumberInput/DecrementButton.svelte
@@ -3,9 +3,9 @@
 </Button>
 
 <script lang="ts">
-  import type { PaperType } from '../../../types';
-  import Button from '../../Button.svelte';
+import type { PaperType } from '../../../types';
+import Button from '../../Button.svelte';
 
-  export let clickHandler: (e: MouseEvent) => any;
-  export let type: PaperType;
+export let clickHandler: (e: MouseEvent) => any;
+export let type: PaperType;
 </script>
